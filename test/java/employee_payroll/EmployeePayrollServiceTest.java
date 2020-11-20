b75f7dc0f3ac5f270299b0b7e305904d7cc0b03a
Sippora04 <sippora.toppo95@gmail.com>
2020-11-21 01:53:26 +0530

    Updated: salary udate using prepared statement

--- test/java/employee_payroll/EmployeePayrollServiceTest.java
+++ test/java/employee_payroll/EmployeePayrollServiceTest.java
@@ -6,16 +6,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class EmployeePayrollServiceTest {
-
+	//UC2
 	@Test
 	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmployeePayrollData> employeePayrollData;
 		employeePayrollData = employeePayrollService.readEmployeePayrollData();
 		Assert.assertEquals(3, employeePayrollData.size());
-
 	}
-
+	//UC3
 	@Test
 	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
@@ -25,5 +24,14 @@ public class EmployeePayrollServiceTest {
 		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
 		Assert.assertTrue(result);
 	}
-
+	//UC4
+	@Test
+    public void givenEmployeePayroll_WhenUpdatedUsingPreparedStatement_ShouldSyncWithDB() throws EmployeePayrollException {
+    	EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+    	List<EmployeePayrollData> employeePayrollData;
+    	employeePayrollData =employeePayrollService.readEmployeePayrollData();
+		employeePayrollService.updateEmployeeSalary("Terisa",3000000.00);
+		boolean result=employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+		Assert.assertTrue(result);
+    }
 }
