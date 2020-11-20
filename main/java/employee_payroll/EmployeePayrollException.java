7badba305505ac14c04e71b7cd785b26406fdea5
Sippora04 <sippora.toppo95@gmail.com>
2020-11-20 20:43:22 +0530

    Added: Create emp-payroll table in database

---
+++ main/java/employee_payroll/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package employee_payroll;
+
+public class EmployeePayrollException extends Exception {
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+}
---
+++ test/java/employee_payroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,18 @@
+package employee_payroll;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmployeePayrollServiceTest {
+
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> employeePayrollData;
+		employeePayrollData = employeePayrollService.readEmployeePayrollData();
+		Assert.assertEquals(2,employeePayrollData.size());
+	}
+
+}
